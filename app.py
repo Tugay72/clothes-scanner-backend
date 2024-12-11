0c04a5f42f026c65a9f00cb23cba24a57d56e753
Tugay <tasimtugay@gmail.com>
2024-12-12 01:45:29 +0300

    a

--- app.py
+++ app.py
@@ -198,5 +198,4 @@ def get_processed_image():
         return jsonify({'error': 'Processed image not found'}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
